3d89eaacf642198ed159e7fb37fa9ac016489afd
shephali09 <hiremathshephali9@gmail.com>
2023-04-06 01:33:41 +0530

    Add: For Given 3 strings find maximum

--- src/com/bridgelabz/findmaximumproblemusinggenerics/MaximumNumber.java
+++ src/com/bridgelabz/findmaximumproblemusinggenerics/MaximumNumber.java
@@ -1,16 +1,16 @@
 package com.bridgelabz.findmaximumproblemusinggenerics;
 
-//Given 3 floats find the maximum numbers 
+//Given 3 strings find the maximum numbers 
 
 public class MaximumNumber {
 
-	public void floatMax(double d, double e, double f) {
-		Double max = d;
-		if (Double.compare(e, max) > 0.0) {
-			max = e;
+	public static void stringMax(String x, String y, String z) {
+		String max = x;
+		if (y.compareTo(max) > 0) {
+			max = y;
 		}
-		if (Double.compare(f, max) > 0.0) {
-			max = f;
+		if (z.compareTo(max) > 0) {
+			max = z;
 		}
 		System.out.println(max);
 	}
@@ -18,7 +18,7 @@ public class MaximumNumber {
 	public static void main(String[] args) {
 
 		MaximumNumber maximumNumber = new MaximumNumber();
-		maximumNumber.floatMax(2.2, 25.8, 100.08);
+		maximumNumber.stringMax("Apple", "Peach", "Banana");
 	}
 
 }
