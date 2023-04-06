fec0606034edca58beaa3c1afde10aaa621e6e4c
shephali09 <hiremathshephali9@gmail.com>
2023-04-06 12:01:51 +0530

    Add: Extend the max method to also print the max to std out using Generic method

--- src/com/bridgelabz/findmaximumproblemusinggenerics/MaximumNumbers.java
+++ src/com/bridgelabz/findmaximumproblemusinggenerics/MaximumNumbers.java
@@ -1,4 +1,5 @@
 package com.bridgelabz.findmaximumproblemusinggenerics;
+
 import java.lang.*;
 
 /*Refactor all the 3 to One Generic Method and find the maximum
@@ -10,12 +11,14 @@ public class MaximumNumbers<M extends Comparable<M>> {
 	private M a;
 	private M b;
 	private M c;
+	private M d;
 
-	public MaximumNumbers(M a, M b, M c) {
+	public MaximumNumbers(M a, M b, M c, M d) {
 		super();
 		this.a = a;
 		this.b = b;
 		this.c = c;
+		this.d = d;
 
 	}
 
@@ -29,14 +32,22 @@ public class MaximumNumbers<M extends Comparable<M>> {
 		if (c.compareTo(max) > 0) {
 			max = c;
 		}
+		if (d.compareTo(max) > 0) {
+			max = d;
+		}
 		return max;
+	}
+
+	public static <M> void printMax(M a, M b, M c, M d, M max) {
+		System.out.println("a value is: " + a + ", b value is: " + b + ", c value is: " + c + ", d value is: " + d
+				+ ", max of three value is: " + max);
 	}
 
 	public static void main(String[] args) {
 
-		MaximumNumbers<Integer> intmax = new MaximumNumbers<Integer>(50, 10, 30);
-		MaximumNumbers<Double> doublemax = new MaximumNumbers<Double>(3.5, 1.5, 1.0);
-		MaximumNumbers<String> stringmax = new MaximumNumbers<String>("apple", "orange", "grape");
+		MaximumNumbers<Integer> intmax = new MaximumNumbers<Integer>(50, 10, 30, 180);
+		MaximumNumbers<Double> doublemax = new MaximumNumbers<Double>(3.5, 1.5, 1.0, 100.08);
+		MaximumNumbers<String> stringmax = new MaximumNumbers<String>("apple", "peach", "grape", "orange");
 
 		System.out.println(intmax.max());
 		System.out.println(doublemax.max());
